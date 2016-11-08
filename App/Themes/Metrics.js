eaa4df6c38d361241e72cbad857c3bbf8960a1aa
Gant Laborde <gantman@gmail.com>
2016-11-08 12:47:26 -0800

    metrics change

--- App/Themes/Metrics.js
+++ App/Themes/Metrics.js
@@ -16,7 +16,7 @@ const metrics = {
   screenWidth: width < height ? width : height,
   screenHeight: width < height ? height : width,
   navBarHeight: (Platform.OS === 'ios') ? 64 : 54,
-  buttonRadius: 14,
+  buttonRadius: 20,
   icons: {
     tiny: 15,
     small: 20,
